d58cdbce92d5b87bdeee6ffb5f52a17e25cd0643
wmsjhappy@gmail.com <clibing>
2020-07-07 17:11:13 +0800

    :sparkles: feat(0.1): add tomcat, maven
    Signed-off-by: wmsjhappy@gmail.com <clibing>

---
+++ alpine-tomcat/build-tomcat.sh
@@ -0,0 +1,20 @@
+#########################################################################
+# File Name: build.sh
+# Author: clibing
+# mail: wmsjhappy@@gmail.com
+# Created Time: 一  7/ 6 15:53:25 2020
+#########################################################################
+#!/bin/bash
+
+source ../version
+
+unset PROJECT_NAME 
+unset IMAGE_BASE_NAME
+
+source ./version
+
+echo "begin build base apache maven."
+docker buildx build --platform=${TARGET_PLATFORM} --build-arg TOMCAT_VERSION=${TOMCAT_VERSION} -t ${IMAGE_BASE_NAME}:${TOMCAT_VERSION} --push -f Dockerfile .
+docker buildx build --platform=${TARGET_PLATFORM} --build-arg TOMCAT_VERSION=${TOMCAT_VERSION} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
+echo "successful build apache maven."
+
