fb0c4f8cc4f05b6dba8c44c214e43a1f329d0593
wmsjhappy@gmail.com <clibing>
2020-07-16 15:45:58 +0800

    :sparkles: feat(0.1): gitlab-runner arm arm64 amd64
    Signed-off-by: wmsjhappy@gmail.com <clibing>

---
+++ alpine-gitlab-runner/build-gitlab-runner.sh
@@ -0,0 +1,20 @@
+#########################################################################
+# File Name: build.sh
+# Author: clibing
+# mail: wmsjhappy@@gmail.com
+# Created Time: 一  7/ 6 15:53:25 2020
+#########################################################################
+#!/bin/bash
+
+source ../version
+
+unset PROJECT_NAME 
+unset IMAGE_BASE_NAME
+
+source ./version
+
+echo "begin build base gitlab runner."
+# echo $GITLAB_RUNNER_VERSION
+docker buildx build --platform=${TARGET_PLATFORM} --build-arg GITLAB_RUNNER_VERSION=${GITLAB_RUNNER_VERSION} -t ${IMAGE_BASE_NAME}:${GITLAB_RUNNER_VERSION} --push -f Dockerfile .
+echo "successful build gitlab runner ."
+
