4b4ab1a6840292020ba4b50a60d13a1aad25a8ff
liubaixun <liubaixun@yinxiang.com>
2022-04-11 17:21:52 +0800

    feat(build): no-cache
    no-cache
    
    Signed-off-by: liubaixun <liubaixun@yinxiang.com>

--- alpine-coredns/build-coredns.sh
+++ alpine-coredns/build-coredns.sh
@@ -16,7 +16,9 @@ source ./version
 echo "begin build base coredns."
 # docker buildx build --no-cache --platform=${TARGET_PLATFORM} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:${COREDNS_VERSION} --push -f Dockerfile .
 # docker buildx build --no-cache --platform=${TARGET_PLATFORM} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
-docker buildx build --platform=${TARGET_PLATFORM} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:${COREDNS_VERSION} --push -f Dockerfile .
-docker buildx build --platform=${TARGET_PLATFORM} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
+# docker buildx build --platform=${TARGET_PLATFORM} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:${COREDNS_VERSION} --push -f Dockerfile .
+# docker buildx build --platform=${TARGET_PLATFORM} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
+docker buildx build --no-cache --platform=${TARGET_PLATFORM} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:${COREDNS_VERSION} --push -f Dockerfile .
+docker buildx build --no-cache --platform=${TARGET_PLATFORM} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
 echo "successful build coredns."
 
