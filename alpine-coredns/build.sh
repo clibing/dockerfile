d0bd87d7fec7457483b9f0a9e557ab6c8d8c940f
clibing <wmsjhappy@gmail.com>
2023-01-31 15:01:33 +0800

    fix(cache): remove cache when latest build
    remove cache when latest build
    
    Signed-off-by: clibing <wmsjhappy@gmail.com>

--- alpine-coredns/build.sh
+++ alpine-coredns/build.sh
@@ -15,6 +15,6 @@ source ./version
 
 echo "begin build base coredns."
 docker buildx build --no-cache --platform=${TARGET_PLATFORM_v8} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:${COREDNS_VERSION} --push -f Dockerfile .
-docker buildx build --no-cache --platform=${TARGET_PLATFORM_v8} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
+docker buildx build --platform=${TARGET_PLATFORM_v8} --build-arg COREDNS_VERSION=${COREDNS_VERSION} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
 echo "successful build coredns."
 
