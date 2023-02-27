b298231a6f2bc180d400265ae539c2aeaa3f2df0
liubaixun <liubaixun@yinxiang.com>
2023-02-27 12:53:52 +0800

    fix(tomcat): tomcat
    tomcat
    
    Signed-off-by: liubaixun <liubaixun@yinxiang.com>

--- alpine-tomcat/build-tomcat.sh
+++ alpine-tomcat/build-tomcat.sh
@@ -14,7 +14,15 @@ unset IMAGE_BASE_NAME
 source ./version
 
 echo "begin build base apache tomcat."
-docker buildx build --platform=${TARGET_PLATFORM} --build-arg TOMCAT_VERSION=${TOMCAT_VERSION} -t ${IMAGE_BASE_NAME}:${TOMCAT_VERSION} --push -f Dockerfile .
-docker buildx build --platform=${TARGET_PLATFORM} --build-arg TOMCAT_VERSION=${TOMCAT_VERSION} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
+docker buildx build --platform=${TARGET_PLATFORM} \
+    --build-arg TOMCAT_VERSION=${TOMCAT_VERSION} \
+    --build-arg JDK_TYPE=${JDK_TYPE} \
+    --build-arg JDK_VERSION=${JDK_VERSION} \
+    -t ${IMAGE_BASE_NAME}:${TOMCAT_VERSION} --push -f Dockerfile .
+docker buildx build --platform=${TARGET_PLATFORM} \
+    --build-arg JDK_TYPE=${JDK_TYPE} \
+    --build-arg JDK_VERSION=${JDK_VERSION} \
+    --build-arg TOMCAT_VERSION=${TOMCAT_VERSION} \
+    -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
 echo "successful build apache tomcat."
 
