97030b6b3b91837f9beb026a78d6559a02a21e68
clibing <wmsjhappy@gmail.com>
2024-01-09 16:36:04 +0800

    fix(fastdfs): add fastdfs build & github.com action
    add fastdfs build & github.com action
    
    Signed-off-by: clibing <wmsjhappy@gmail.com>

--- alpine-fastdfs/build-fastdfs.sh
+++ alpine-fastdfs/build-fastdfs.sh
@@ -14,7 +14,20 @@ unset IMAGE_BASE_NAME
 source ./version
 
 echo "begin build base fastdfs."
-docker buildx build --platform=${TARGET_PLATFORM} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
-docker buildx build --platform=${TARGET_PLATFORM} -t ${IMAGE_BASE_NAME}:${VERSION} --push -f Dockerfile .
+
+docker buildx build --platform=${TARGET_PLATFORM} \ 
+    --build-arg NGINX_VERSION=1.24.0 \
+    --build-arg FASTDFS_NGX_MODULE=V1.24 \
+    --build-arg FAST_DFS_VERSION=V6.11.0 \
+    --build-arg LIBFASTCOMMON_VERSION=V1.0.71 \
+    -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
+
+docker buildx build --platform=${TARGET_PLATFORM} \
+    --build-arg NGINX_VERSION=1.24.0 \
+    --build-arg FASTDFS_NGX_MODULE=V1.24 \
+    --build-arg FAST_DFS_VERSION=V6.11.0 \
+    --build-arg LIBFASTCOMMON_VERSION=V1.0.71 \
+    -t ${IMAGE_BASE_NAME}:${VERSION} --push -f Dockerfile .
+
 echo "successful build fastdfs."
 
