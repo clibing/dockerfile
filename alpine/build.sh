badf6e9b80cd60686e749736054b3e753ccadb90
vps <vpm@linux.com>
2024-07-23 14:48:22 +0800

    add branch hub.linuxcrypt.cn

--- alpine/build.sh
+++ alpine/build.sh
@@ -12,6 +12,6 @@ source ./version
 export TARGET_PLATFORM="linux/386,${TARGET_PLATFORM}"
 
 echo "begin build base alpine."
-docker buildx build --no-cache --platform=${TARGET_PLATFORM} --build-arg ALPINE_VERSION=${ALPINE_VERSION} -t ${IMAGE_BASE_NAME}:${ALPINE_VERSION} --push -f Dockerfile .
-docker buildx build --platform=${TARGET_PLATFORM} --build-arg ALPINE_VERSION=${ALPINE_VERSION} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
+docker buildx build --no-cache --platform=${TARGET_PLATFORM_ALL} --build-arg ALPINE_VERSION=${ALPINE_VERSION} -t ${CUSTOM_PREFIX}${IMAGE_BASE_NAME}:${ALPINE_VERSION} --push -f Dockerfile .
+# docker buildx build --platform=${TARGET_PLATFORM} --build-arg ALPINE_VERSION=${ALPINE_VERSION} -t ${IMAGE_BASE_NAME}:latest --push -f Dockerfile .
 echo "successful build base alpine."
